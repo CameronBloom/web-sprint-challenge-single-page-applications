03a7f0d45c29aa4fdc3f286ded8528af946ba8d7
Cameron Morgan <cameron.morgan@skiff.com>
2023-03-08 18:47:09 -0800

    Completed the 'Confirmation' page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home.js"
 import Help from "./components/Help";
+import Confirmation from "./components/Confirmation.js";
 
 const App = () => {
   return (
@@ -11,12 +12,14 @@ const App = () => {
         <nav class="nav-bar">
           <Link to="/">Home Screen</Link>&nbsp;
           <Link to="/help">Help Screen</Link>&nbsp;
+          <Link to="/confirm">Confirmation Screen</Link>&nbsp;
         </nav>
       </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/help" element={<Help />} />
+        <Route path="/confirm" element={<Confirmation />} />
       </Routes>
     </>
   );
--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,5 +1,17 @@
+import imgPizza from "../images/Pizza.jpg"
+import imgPizzaDog from "../images/PizzaDog.jpeg"
+
 export default function Confirmation(props) {
   return (
-    true
+    <>
+      <div class="img-container">
+        <img class="img" src={imgPizza} alt="good looking pizza"></img>
+        <div class="img-text"><h3>Congrats! Pizza is on its way</h3></div>
+      </div>
+      <div class="enjoy">
+        <div class="enjoy-text"><h5>Enjoy this Dog with Pizza</h5></div>
+        <img class="enjoy-img" src={imgPizzaDog} alt="happy dog with pizza in mouth"/>
+      </div>
+    </>
   )
 }
