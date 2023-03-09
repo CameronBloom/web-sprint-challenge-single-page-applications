6c2d56fb4f93e72c241f550b25c25e2af2464e5f
Cameron Morgan <cameron.morgan@skiff.com>
2023-03-09 02:56:08 -0800

    Added state functionality, error handling, change handling and effect events.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,51 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home.js"
 import Help from "./components/Help";
 import Form from "./components/Form.js";
+import * as yup from 'yup';
+import { useState, useEffect } from "react";
 // import Confirmation from "./components/Confirmation.js";
 
+const schema = yup.object().shape({
+  name: yup.string().required('name must be at least 2 characters').min(2, 'name must be at least 2 characters'),
+  size: yup.string().required('must select a pizza size'),
+  sauce: yup.string().required('must select a pizza sauce'),
+  special: yup.string()
+})
+
 const App = () => {
+  
+  // states
+  const [form, setForm] = useState({ name:"", size:"per", sauce:"", toppings:[], subs:false, special:"" })
+  // const [order, setOrder] = useState({ quantity: 1 })
+  const [disabled, setDisabled] = useState(true);
+  // const [substitute, setSubstitute] = useState(false);
+  const [errors, setErrors] = useState({ name: "" })
+
+
+  const handleErrors = (name, value) => {
+    yup.reach(schema, name).validate(value)
+      .then(() => {
+        setErrors({ ...errors, [name]: '' })
+      })
+      .catch(err => {
+        setErrors({ ...errors, [name]: err.errors[0] })
+      })
+  }
+
+  const handleChange = (event) => {
+    const { checked, value, name, type } = event.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    handleErrors(name, valueToUse);
+    setForm({ ...form, [name]: valueToUse});
+    console.log(form);
+  }
+
+  // triggers every time the "form" updates
+  useEffect(() => {
+    schema.isValid(form)
+      .then(valid => setDisabled(!valid))
+      .catch(err => console.error(err))
+  }, [form])
 
   return (
     <>
@@ -21,7 +63,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/help" element={<Help />} />
-        <Route path="/pizza" element={<Form />} />
+        <Route path="/pizza" element={<Form form={form} errors={errors} disabled={disabled} handleChange={handleChange} handleErrors={handleErrors} />} />
         {/* <Route path="/confirm" element={<Confirmation />} /> */}
       </Routes>
     </>
--- src/components/Form.js
+++ src/components/Form.js
@@ -24,66 +24,132 @@ export default function Form(props) {
         <img id="pizza-img" src={imgPizza} alt="good looking pizza" />
         <div id="pizza-desc"><h6>Build Your Own Pizza</h6></div>
         <form id="pizza-form">
-          <label for="name">
+          <label htmlFor="name">
             <p className="bold">Enter Your Name</p>
-            <p>Required</p>
+            <div className="error"><p>Required...</p>&nbsp;<p>{props.errors.name}</p></div>
           </label>
           <input 
             type="text"
             id="name-input"
             name="name"
-            // value={props.form["firstName"]}
-            // onChange={props.handleChange}
+            value={props.form["name"]}
+            onChange={props.handleChange}
           />
-
-
-          <label for="size">
+          <label htmlFor="size">
             <p className="bold">Choice of Size</p>
             <p>Required</p>
           </label>
-          <label for="sauce">
+          <select name="size" id="size-dropdown" onChange={props.handleChange} value={props.form["size"]}>
+            <option value="per">Personal - 10"</option>
+            <option value="sm">Small - 12"</option>
+            <option value="md">Medium - 14"</option>
+            <option value="lg">Large - 16"</option>
+            <option value="xlg">Extra Large - 18"</option>
+          </select>
+          <label htmlFor="sauce">
             <p className="bold">Choice of Sauce</p>
             <p>Required</p>
           </label>
-          <div id="radio-container">
+          <div id="radio-container" onChange={props.handleChange} value={props.form["sauce"]}>
             <div className="radio-row">
               <input type="radio" id="sauce1" name="sauce" value="Original Red" />
-              <label for="sauce1" className="sauce-label">Original Red</label>
+              <label htmlFor="sauce1" className="sauce-label">Original Red</label>
             </div>
             <div className="radio-row">
               <input type="radio" id="sauce2" name="sauce" value="Garlic Ranch" />
-              <label for="sauce2">Garlic Ranch</label>
+              <label htmlFor="sauce2">Garlic Ranch</label>
             </div>
             <div className="radio-row">
               <input type="radio" id="sauce3" name="sauce" value="BBQ Sauce" />
-              <label for="sauce3">BBQ Sauce</label>
+              <label htmlFor="sauce3">BBQ Sauce</label>
             </div>
             <div className="radio-row">
               <input type="radio" id="sauce4" name="sauce" value="Spinach Alfredo" />
-              <label for="sauce4">Spinach Alfredo</label>
+              <label htmlFor="sauce4">Spinach Alfredo</label>
             </div>
           </div>
 
-
-
-          <label for="toppings">
+          <label htmlFor="toppings">
             <p className="bold">Add Toppings</p>
             <p>Choose up to 10.</p>
           </label>
-          <label for="substitute">
+          <div id="checkbox-container">
+            <div id="checkbox-left">
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping1" name="topping1" value="Pepperoni" />
+                <label htmlFor="topping1">Pepperoni</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping2" name="topping2" value="Sausage" />
+                <label htmlFor="topping2">Sausage</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping3" name="topping3" value="Canadian Bacon" />
+                <label htmlFor="topping3">Canadian Bacon</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping4" name="topping4" value="Spicy Italian Sausage" />
+                <label htmlFor="topping4">Spicy Italian Sausage</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping5" name="topping5" value="Grilled Chickens" />
+                <label htmlFor="topping5">Grilled Chickens</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping6" name="topping6" value="Onions" />
+                <label htmlFor="topping6">Onions</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping7" name="topping7" value="Green Pepper" />
+                <label htmlFor="topping7">Green Pepper</label>
+              </div>
+            </div>
+            <div id="checkbox-right">
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping8" name="topping8" value="Diced Tomato" />
+                <label htmlFor="topping8">Diced Tomato</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping9" name="topping9" value="Diced Tomato" />
+                <label htmlFor="topping9">Black Olives</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping10" name="topping10" value="Roasted Garlic" />
+                <label htmlFor="topping10">Roasted Garlic</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping11" name="topping11" value="Artichoke Hearts" />
+                <label htmlFor="topping11">Artichoke Hearts</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping12" name="topping12" value="Three Cheese" />
+                <label htmlFor="topping12">Three Cheese</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping13" name="topping13" value="Pineapple" />
+                <label htmlFor="topping13">Pineapple</label>
+              </div>
+              <div className="checkbox-row">
+                <input type="checkbox" id="topping14" name="topping14" value="Extra Cheese" />
+                <label htmlFor="topping14">Extra Cheese</label>
+              </div>
+            </div>
+          </div>
+
+          <label htmlFor="substitute">
             <p className="bold">Choice of Substitute</p>
             <p>Choose up to 1.</p>
           </label>
-          <label for="special-text">
+          <label htmlFor="special-text">
             <p className="bold">Special Instructions</p>
           </label>
           <div id="special-container">
             <textarea 
               type="text"
               id="special-text"
-              name="special-text"
-              // value={props.form["firstName"]}
-              // onChange={props.handleChange}
+              name="special"
+              value={props.form["special"]}
+              onChange={props.handleChange}
             />
           </div>
  
@@ -107,6 +173,7 @@ export default function Form(props) {
             <div id="pizza-counter">1</div>
             <div id="pizza-submit">Add to Order</div>
           </div> */}
+          <button disabled={props.disabled}>Submit</button>
         </form>
       </div>
 
